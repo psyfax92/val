e33fa750bb7e9a47f7c10b084615d9da1fb9c6d2
psyfax92 <62992299+psyfax92@users.noreply.github.com>
2020-04-01 13:21:30 +0200

    Add files via upload

---
+++ List.h
@@ -0,0 +1,41 @@
+#ifndef List_h
+#define List_h
+
+#include <initializer_list>
+
+class List
+{
+public:
+    List();
+    List(List const &);
+    List(List &&) noexcept;
+    List(std::initializer_list<int>);
+    
+    List & operator=(List const &)&;
+    List & operator=(List &&)& noexcept;
+    
+    void push_front(int);
+    void push_back(int);
+    
+    int back() const noexcept;
+    int & back() noexcept;
+    
+    int front() const noexcept;
+    int & front() noexcept;
+    
+    int & at(int idx);
+    int const & at(int idx) const;
+    
+    int size() const noexcept;
+    bool empty() const noexcept;
+    
+    void swap(List & other) noexcept;
+    
+private:
+    struct Node;
+    Node * head {};
+    Node * tail {};
+    int sz {};
+};
+
+#endif
